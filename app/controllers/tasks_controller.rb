2cb4fa97819b7f2b47753a2948545dec32277dc0
Antoine Aube <aaube3@gmail.com>
2023-08-17 21:16:36 -0600

    complete task manager

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,18 +7,38 @@ class TasksController < ApplicationController
 
   end
   def create
-    # require 'pry';binding.pry
     task = Task.new({
-      title: params[:title],
-      description: params[:description]
+    title: params[:title],
+    description: params[:description]
     })
-    # require 'pry';binding.pry
+
     task.save
-    # require 'pry';binding.pry
+
     redirect_to '/tasks'
   end
 
   def show
+    # require 'pry';binding.pry
     @task = Task.find(params[:id])
   end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.update({
+      title: params[:title],
+      description: params[:description]
+      })
+    task.save
+    redirect_to "/tasks/#{task.id}"
+  end
+
+  def destroy
+    Task.destroy(params[:id])
+    redirect_to '/tasks'
+  end
+
 end
